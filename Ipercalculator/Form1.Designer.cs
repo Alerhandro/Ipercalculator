ee2a02df77e4b2361f9c3e781653634ead116f88
Byo <alerhandrojp@gmail.com>
2022-11-16 23:50:32 -0400

    Adicionar arquivos de projeto.

---
+++ Ipercalculator/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Ipercalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.um = new System.Windows.Forms.Button();
+            this.dois = new System.Windows.Forms.Button();
+            this.tres = new System.Windows.Forms.Button();
+            this.quatro = new System.Windows.Forms.Button();
+            this.cinco = new System.Windows.Forms.Button();
+            this.seis = new System.Windows.Forms.Button();
+            this.sete = new System.Windows.Forms.Button();
+            this.oito = new System.Windows.Forms.Button();
+            this.nove = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.dec = new System.Windows.Forms.Button();
+            this.mais = new System.Windows.Forms.Button();
+            this.menos = new System.Windows.Forms.Button();
+            this.vez = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.iqual = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // um
+            // 
+            resources.ApplyResources(this.um, "um");
+            this.um.Name = "um";
+            this.um.UseVisualStyleBackColor = true;
+            this.um.Click += new System.EventHandler(this.button_Click);
+            // 
+            // dois
+            // 
+            resources.ApplyResources(this.dois, "dois");
+            this.dois.Name = "dois";
+            this.dois.UseVisualStyleBackColor = true;
+            this.dois.Click += new System.EventHandler(this.button_Click);
+            // 
+            // tres
+            // 
+            resources.ApplyResources(this.tres, "tres");
+            this.tres.Name = "tres";
+            this.tres.UseVisualStyleBackColor = true;
+            this.tres.Click += new System.EventHandler(this.button_Click);
+            // 
+            // quatro
+            // 
+            resources.ApplyResources(this.quatro, "quatro");
+            this.quatro.Name = "quatro";
+            this.quatro.UseVisualStyleBackColor = true;
+            this.quatro.Click += new System.EventHandler(this.button_Click);
+            // 
+            // cinco
+            // 
+            resources.ApplyResources(this.cinco, "cinco");
+            this.cinco.ForeColor = System.Drawing.Color.Black;
+            this.cinco.Name = "cinco";
+            this.cinco.UseVisualStyleBackColor = true;
+            this.cinco.Click += new System.EventHandler(this.button_Click);
+            // 
+            // seis
+            // 
+            resources.ApplyResources(this.seis, "seis");
+            this.seis.Name = "seis";
+            this.seis.UseVisualStyleBackColor = true;
+            this.seis.Click += new System.EventHandler(this.button_Click);
+            // 
+            // sete
+            // 
+            resources.ApplyResources(this.sete, "sete");
+            this.sete.Name = "sete";
+            this.sete.UseVisualStyleBackColor = true;
+            this.sete.Click += new System.EventHandler(this.button_Click);
+            // 
+            // oito
+            // 
+            resources.ApplyResources(this.oito, "oito");
+            this.oito.Name = "oito";
+            this.oito.UseVisualStyleBackColor = true;
+            this.oito.Click += new System.EventHandler(this.button_Click);
+            // 
+            // nove
+            // 
+            resources.ApplyResources(this.nove, "nove");
+            this.nove.Name = "nove";
+            this.nove.UseVisualStyleBackColor = true;
+            this.nove.Click += new System.EventHandler(this.button_Click);
+            // 
+            // zero
+            // 
+            resources.ApplyResources(this.zero, "zero");
+            this.zero.Name = "zero";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.button_Click);
+            // 
+            // dec
+            // 
+            resources.ApplyResources(this.dec, "dec");
+            this.dec.Name = "dec";
+            this.dec.UseVisualStyleBackColor = true;
+            this.dec.Click += new System.EventHandler(this.button_Click);
+            // 
+            // mais
+            // 
+            resources.ApplyResources(this.mais, "mais");
+            this.mais.Name = "mais";
+            this.mais.UseVisualStyleBackColor = true;
+            this.mais.Click += new System.EventHandler(this.operador_click);
+            // 
+            // menos
+            // 
+            resources.ApplyResources(this.menos, "menos");
+            this.menos.Name = "menos";
+            this.menos.UseVisualStyleBackColor = true;
+            this.menos.Click += new System.EventHandler(this.operador_click);
+            // 
+            // vez
+            // 
+            resources.ApplyResources(this.vez, "vez");
+            this.vez.Name = "vez";
+            this.vez.UseVisualStyleBackColor = true;
+            this.vez.Click += new System.EventHandler(this.operador_click);
+            // 
+            // div
+            // 
+            resources.ApplyResources(this.div, "div");
+            this.div.Name = "div";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.operador_click);
+            // 
+            // iqual
+            // 
+            resources.ApplyResources(this.iqual, "iqual");
+            this.iqual.Name = "iqual";
+            this.iqual.UseVisualStyleBackColor = true;
+            this.iqual.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            resources.ApplyResources(this.button17, "button17");
+            this.button17.Name = "button17";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            resources.ApplyResources(this.button18, "button18");
+            this.button18.Name = "button18";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // resultado
+            // 
+            resources.ApplyResources(this.resultado, "resultado");
+            this.resultado.BackColor = System.Drawing.Color.White;
+            this.resultado.Name = "resultado";
+            this.resultado.TextChanged += new System.EventHandler(this.resultado_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.iqual);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.vez);
+            this.Controls.Add(this.menos);
+            this.Controls.Add(this.mais);
+            this.Controls.Add(this.dec);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nove);
+            this.Controls.Add(this.oito);
+            this.Controls.Add(this.sete);
+            this.Controls.Add(this.seis);
+            this.Controls.Add(this.cinco);
+            this.Controls.Add(this.quatro);
+            this.Controls.Add(this.tres);
+            this.Controls.Add(this.dois);
+            this.Controls.Add(this.um);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button um;
+        private System.Windows.Forms.Button dois;
+        private System.Windows.Forms.Button tres;
+        private System.Windows.Forms.Button quatro;
+        private System.Windows.Forms.Button cinco;
+        private System.Windows.Forms.Button seis;
+        private System.Windows.Forms.Button sete;
+        private System.Windows.Forms.Button oito;
+        private System.Windows.Forms.Button nove;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button dec;
+        private System.Windows.Forms.Button mais;
+        private System.Windows.Forms.Button menos;
+        private System.Windows.Forms.Button vez;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button iqual;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ipercalculator/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ipercalculator
+{
+    public partial class Form1 : Form
+    {
+        Double value = 0;
+        String operador = "";
+        bool operador_presionado = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((resultado.Text == "0") || (operador_presionado))
+                resultado.Clear();
+            
+            Button b = (Button)sender;
+            if (b.Text == ".")
+            {
+                if(!resultado.Text.Contains("."))
+                    resultado.Text = resultado.Text + b.Text;
+            }
+            else
+              resultado.Text = resultado.Text +b.Text;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            resultado.Text = "0";
+        }
+
+        private void operador_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (value != 0)
+            {
+                iqual.PerformClick();
+                operador_presionado = true;
+                operador = b.Text;
+                label1.Text = value + " " + operador;
+            }
+            else
+            {
+                operador = b.Text;
+                value = Double.Parse(resultado.Text);
+                operador_presionado = true;
+                label1.Text = value + " " + operador;
+            }
+        }
+
+        private void resultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+            switch (operador)
+            {
+                case "+":
+                    resultado.Text = (value + Double.Parse(resultado.Text)).ToString();
+                    break;
+
+                case "-":
+                    resultado.Text = (value - Double.Parse(resultado.Text)).ToString();
+                    break;
+
+                case "*":
+                    resultado.Text = (value * Double.Parse(resultado.Text)).ToString();
+                    break;
+
+                case "/":
+                    resultado.Text = (value / Double.Parse(resultado.Text)).ToString();
+                    break;
+
+                default:
+                    break;
+
+            }//final switch
+            value = Int32.Parse(resultado.Text);
+            operador = "";
+            operador_presionado = false;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch(e.KeyChar.ToString())
+            {
+                case "0":
+                    zero.PerformClick();
+                    break;
+                case "1":
+                    um.PerformClick();
+                    break;
+                case "2":
+                    dois.PerformClick();
+                    break;
+                case "3":
+                    tres.PerformClick();
+                    break;
+                case "4":
+                    quatro.PerformClick();
+                    break;
+                case "5":
+                    cinco.PerformClick();
+                    break;
+                case "6":
+                    seis.PerformClick();
+                    break;
+                case "7":
+                    sete.PerformClick();
+                    break;
+                case "8":
+                    oito.PerformClick();
+                    break;
+                case "9":
+                    nove.PerformClick();
+                    break;
+                case "/":
+                    div.PerformClick();
+                    break;
+                case "+":
+                    mais.PerformClick();
+                    break;
+                case "-":
+                    menos.PerformClick();
+                    break;
+                case "*":
+                    vez.PerformClick();
+                    break;
+                case "=":
+                    iqual.PerformClick();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
